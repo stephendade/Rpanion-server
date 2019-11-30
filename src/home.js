823356e0a2f918399509398f3f06cf46667a0081
Stephen Dade <stephen_dade@hotmail.com>
2019-11-30 14:53:25 +1100

    Home: Add some text

--- src/home.js
+++ src/home.js
@@ -4,10 +4,12 @@ import { Helmet } from 'react-helmet'
 function Home() {
   return (
     <div>
-		<Helmet>
+        <Helmet>
           <title>The Home Page</title>
         </Helmet>
       <h2>Home</h2>
+      <p>Welcome to the Rpanion-server home page</p>
+      <p>Use the links on the left to configure the system</p>
     </div>
   );
 }
