dc2e5bce0ba8c125b38066248d5efbca5a8f07df
Stephen Dade <stephen_dade@hotmail.com>
2019-11-30 14:53:01 +1100

    Tests: Add initial for front end

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-it('renders without crashing', () => {
+import About from './about.js';
+import Home from './home.js';
+import NetworkConfig from './networkconfig.js';
+import Video from './video.js';
+import FCConfig from './flightcontroller.js';
+
+it('homepage renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Home />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('about page renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<About />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('networkconfig page renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<NetworkConfig />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('video page renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Video />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('flightcontroller page renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<FCConfig />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
