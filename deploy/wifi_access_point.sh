ee3fc10bbe162ba79d54a05cb79486c2e71f9ebf
Stephen Dade <stephen_dade@hotmail.com>
2019-12-01 17:45:43 +1100

    Deploy: Added script for initial Wifi hotspot

---
+++ deploy/wifi_access_point.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# this script sets up the wifi access point
+
+# Create Access Point
+APNAME="WiFiAP"
+SSID="rpanion"
+KEY="rpanion"
+
+IFNAME=wlan0
+sudo nmcli connection add type wifi ifname $IFNAME con-name $APNAME ssid $SSID
+sudo nmcli connection modify $APNAME 802-11-wireless.mode ap
+sudo nmcli connection modify $APNAME 802-11-wireless.band bg
+sudo nmcli connection modify $APNAME ipv4.method shared
+sudo nmcli connection modify $APNAME wifi-sec.key-mgmt wpa-psk
+sudo nmcli connection modify $APNAME ipv4.addresses 10.0.1.1/24
+sudo nmcli connection modify $APNAME wifi-sec.psk "$KEY"
+sudo nmcli connection modify $APNAME 802-11-wireless-security.group ccmp
+sudo nmcli connection modify $APNAME 802-11-wireless-security.pairwise ccmp
+sudo nmcli connection up $APNAME
+
