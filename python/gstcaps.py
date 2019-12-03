cc74d38627b7ec9e139c75cc6bca610dab144d85
Stephen Dade <stephen_dade@hotmail.com>
2019-12-03 20:48:12 +1100

    Video: Better detection of Ras Pi Cameras

--- python/gstcaps.py
+++ python/gstcaps.py
@@ -22,35 +22,36 @@ for device in devices:
     name = device.get_properties().get_string("v4l2.device.card")
     caps = []
 
-    #Get better name for camera
-    if "UVC Camera (" in name:
-        vendorproduct = name.split("(")[1].split(")")[0]
-        import subprocess
-        process = subprocess.Popen(['lsusb', '-d', vendorproduct], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
-        if stderr == b'' and vendorproduct in stdout.decode("utf-8"):
-            name = stdout.decode("utf-8").split(vendorproduct)[1].strip()
+    # Check if it's a Rpi Cam V2, if so manually add modes for hardware encoding
+    # Also change path to tell rtsp-server to use specific ras pi cam driver
+    if "mmal service" in name:
+        caps = []
+        caps.append({'value': 0, 'label': "1920x1080", 'height': 1080, 'width': 1920, 'format': 'video/x-h264'})
+        caps.append({'value': 1, 'label': "1640x922", 'height': 922, 'width': 1640, 'format': 'video/x-h264'})
+        caps.append({'value': 2, 'label': "1280x720", 'height': 720, 'width': 1280, 'format': 'video/x-h264'})
+        caps.append({'value': 3, 'label': "640x480", 'height': 480, 'width': 640, 'format': 'video/x-h264'})
+        name = "Raspberry Pi Camera (V2)"
+        path = "rpicam"
 
-    capsGST = device.get_caps()
-    
-    # enumerate available resolutions
-    for i in range(capsGST.get_size()):
-        structure = capsGST.get_structure(i)
-        # Check if it's a Rpi Cam V2, if so manually add modes for hardware encoding
-        # Also change path to tell rtsp-server to use specific ras pi cam driver
-        if structure.to_string() == "video/x-h264, stream-format=(string)byte-stream, alignment=(string)au, width=(int)[ 32, 3280, 2 ], height=(int)[ 32, 2464, 2 ], framerate=(fraction)[ 1/1, 90/1 ];":
-            caps = []
-            caps.append({'value': 0, 'label': "1920x1080", 'height': 1080, 'width': 1920, 'format': 'video/x-h264'})
-            caps.append({'value': 1, 'label': "1640x922", 'height': 922, 'width': 1640, 'format': 'video/x-h264'})
-            caps.append({'value': 2, 'label': "1280x720", 'height': 720, 'width': 1280, 'format': 'video/x-h264'})
-            caps.append({'value': 3, 'label': "640x480", 'height': 480, 'width': 640, 'format': 'video/x-h264'})
-            name = "Raspberry Pi Camera (V2)"
-            path = "rpicam"
-            break
-        if structure.get_name() in ['video/x-raw', 'video/x-h264'] :
-            width = structure.get_int('width').value
-            height = structure.get_int('height').value
-            caps.append({'value': i, 'label': "{0}x{1}".format(width, height), 'height': int(height), 'width': int(width), 'format': structure.get_name()})
+    else:
+        #Get better name for camera
+        if "UVC Camera (" in name:
+            vendorproduct = name.split("(")[1].split(")")[0]
+            import subprocess
+            process = subprocess.Popen(['lsusb', '-d', vendorproduct], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, stderr = process.communicate()
+            if stderr == b'' and vendorproduct in stdout.decode("utf-8"):
+                name = stdout.decode("utf-8").split(vendorproduct)[1].strip()
+
+        capsGST = device.get_caps()
+        
+        # enumerate available resolutions
+        for i in range(capsGST.get_size()):
+            structure = capsGST.get_structure(i)
+            if structure.get_name() in ['video/x-raw', 'video/x-h264'] :
+                width = structure.get_int('width').value
+                height = structure.get_int('height').value
+                caps.append({'value': i, 'label': "{0}x{1}".format(width, height), 'height': int(height), 'width': int(width), 'format': structure.get_name()})
             
     retDevices.append({'value': path, 'label': name, 'caps': caps})
 
