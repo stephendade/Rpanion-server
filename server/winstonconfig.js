1186230c2f0614410a6db3e8f3452a58490a53a4
Stephen Dade <stephen_dade@hotmail.com>
2020-06-09 17:18:15 +1000

    Winston: Lint config file

--- server/winstonconfig.js
+++ server/winstonconfig.js
@@ -1,6 +1,6 @@
-var winston = require('winston');
-const path = require('path');
-var appRoot = require('app-root-path');
+var winston = require('winston')
+const path = require('path')
+var appRoot = require('app-root-path')
 
 // define the custom settings for each transport (file, console)
 var options = {
@@ -11,32 +11,32 @@ var options = {
     json: true,
     maxsize: 5242880, // 5MB
     maxFiles: 5,
-    colorize: false,
-  },
-};
+    colorize: false
+  }
+}
 
 // Return the last folder name in the path and the calling
 // module's filename.
-const getLabel = function(callingModule) {
-  const parts = callingModule.filename.split(path.sep);
-  return path.join(parts[parts.length - 2], parts.pop());
-};
+const getLabel = function (callingModule) {
+  const parts = callingModule.filename.split(path.sep)
+  return path.join(parts[parts.length - 2], parts.pop())
+}
 
 // instantiate a new Winston Logger with the settings defined above
-function logger(callingModule) {
-    return new winston.createLogger({
-        format: winston.format.combine(
-            winston.format.label({ label: getLabel(callingModule) }),
-            winston.format.timestamp({
-                format: 'YYYY-MM-DD HH:mm:ss'
-            }),
-            winston.format.printf(info => `${info.timestamp} ${info.level} [${info.label}]: ${info.message}`)
-        ),
-        transports: [
-            new winston.transports.File(options.file),
-        ],
-        exitOnError: false, // do not exit on handled exceptions
-    });
+function logger (callingModule) {
+  return new winston.createLogger({
+    format: winston.format.combine(
+      winston.format.label({ label: getLabel(callingModule) }),
+      winston.format.timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss'
+      }),
+      winston.format.printf(info => `${info.timestamp} ${info.level} [${info.label}]: ${info.message}`)
+    ),
+    transports: [
+      new winston.transports.File(options.file)
+    ],
+    exitOnError: false // do not exit on handled exceptions
+  })
 };
 
-module.exports = logger;
+module.exports = logger
