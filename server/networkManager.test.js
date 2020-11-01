73036e02bf9384c74faf0d6ad9211600f7ea58b9
Stephen Dade <stephen_dade@hotmail.com>
2020-11-01 11:52:39 +1100

    Tests: Add a few networkManager tests

---
+++ server/networkManager.test.js
@@ -0,0 +1,22 @@
+var assert = require('assert')
+var appRoot = require('app-root-path')
+const networkManager = require('./networkManager');
+
+describe('Network Functions', function () {
+
+  it('#networkmanagergetAdapters()', function (done) {
+    // Getting a list of adapters
+    networkManager.getAdapters(function (err, netDeviceList) {
+      assert.equal(err, null)
+      done()
+    });
+  })
+
+  it('#networkmanagergetConnections()', function (done) {
+    // Getting a list of adapters
+    networkManager.getConnections(function (err, netConnectionList) {
+      assert.equal(err, null)
+      done()
+    });
+  })
+})
