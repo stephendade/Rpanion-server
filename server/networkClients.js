0a78f72061554b05df2b1d2c391e5200aafd2ffe
Stephen Dade <stephen_dade@hotmail.com>
2020-12-07 19:56:42 +1100

    fix

--- server/networkClients.js
+++ server/networkClients.js
@@ -1,4 +1,4 @@
-const { exec } = require('child_process')
+const { exec, execSync } = require('child_process')
 var winston = require('./winstonconfig')(module)
 
 function getClients (callback) {
@@ -8,10 +8,9 @@ function getClients (callback) {
     if (stderr) {
       console.error(`exec error: ${error}`)
       winston.error('Error in getClients() ', { message: stderr })
-      return callback(stderr, null, null)
+      return callback(stderr.toString(), null, null)
     } else {
       var allConns = stdout.split('\n')
-      var hasRet = false
       // stdout.split("\n").forEach(function (item) {
       for (var i = 0, len = allConns.length; i < len; i++) {
         var item = allConns[i]
@@ -19,50 +18,37 @@ function getClients (callback) {
         var device = connection[3]
         if (connection[2] === '802-11-wireless' && (connection[3] !== '' && connection[3] !== '--')) {
           // get connection details
-          exec('nmcli -s -t -f 802-11-wireless.mode connection show ' + connection[1], (error, stdout, stderr) => {
-            if (stderr) {
-              console.error(`exec error: ${error}`)
-              winston.error('Error in getClients() inter2 ', { message: stderr })
-              hasRet = true
-              return callback(stderr, null, null)
-            }
-            var modeline = stdout.split('\n')[0].split(':')[1]
+          try {
+            var output = execSync('nmcli -s -t -f 802-11-wireless.mode connection show ' + connection[1])
+            var modeline = output.toString().split('\n')[0].split(':')[1]
             if (modeline === 'ap') {
               // Stored in sudo cat /var/lib/NetworkManager/dnsmasq-wlan0.leases
               // 1606808691 34:7d:f6:65:b1:1b 10.0.2.117 l5411 01:34:7d:f6:65:b1:1b
               // we have an active AP
               var allclients = []
-              exec('sudo cat /var/lib/NetworkManager/dnsmasq-' + device + '.leases', (error, stdout, stderr) => {
-                if (stderr) {
-                  console.error(`exec error: ${error}`)
-                  winston.error('Error in getClients() inter ', { message: stderr })
-                  hasRet = true
-                  return callback(stderr, null, null)
-                } else {
-                  var allleases = stdout.split('\n')
-                  for (var j = 0, len = allleases.length; j < len; j++) {
-                    var details = allleases[j].split(' ')
-                    if (details.length !== 5) {
-                      winston.error('Bad lease ', { message: details })
-                      return callback('Bad lease', connection, [])
-                    }
-                    var ip = details[2]
-                    var mac = details[1]
-                    var hostname = details[3]
-                    allclients.push({ ip: ip, mac: mac, hostname: hostname })
-                  }
+              var out = execSync('sudo cat /var/lib/NetworkManager/dnsmasq-' + device + '.leases')
+              var allleases = out.toString().split('\n')
+              for (var j = 0, lenn = allleases.length; j < lenn; j++) {
+                var details = allleases[j].split(' ')
+                if (details.length !== 5) {
+                  winston.error('Bad lease ', { message: details })
+                  return callback('Bad lease', connection, [])
                 }
-                hasRet = true
-                return callback(null, connection, allclients)
-              })
+                var ip = details[2]
+                var mac = details[1]
+                var hostname = details[3]
+                allclients.push({ ip: ip, mac: mac, hostname: hostname })
+              }
+              return callback(null, connection, allclients)
             }
-          })
+          } catch (e) {
+            winston.error('Error in getClients() inter2 ', { message: e })
+            return callback(e.toString(), null, null)
+          }
         }
       }
-      // no AP
-      if (!hasRet) {
-        return callback(null, '', null)
-      }
+      // no AP, as we've not returned yet
+      return callback(null, '', null)
     }
   })
 }
