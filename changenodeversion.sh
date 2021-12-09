2f442c7b30889996a44bd539c8071b025a1aafd7
Stephen Dade <stephen_dade@hotmail.com>
2021-12-09 20:02:07 +1100

    Build: Add script to change nodejs version

---
+++ changenodeversion.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Change to a different nodejs version using n:
+# Requires "npm install -g n"
+
+# List all installed versions
+# n ls
+
+# Change to specific version:
+sudo n 11.15.0
