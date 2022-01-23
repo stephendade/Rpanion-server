83beb92f6ba40dbb484c7ce5f3fd37fd8ef4bc97
lorenzing <lorenzo.franco94@gmail.com>
2022-01-23 12:26:22 +0100

    Add Rpanion-updater script

--- deploy/RasPi2-3-4-deploy.sh
+++ deploy/RasPi2-3-4-deploy.sh
@@ -55,6 +55,11 @@ cd ../../deploy
 ## mavlink-router
 ./build_mavlinkrouter.sh
 
+## Rpanion-updater
+cd /etc/init.d
+sudo wget https://raw.githubusercontent.com/lorenzing/Rpanion-updater/main/rpanion-updater
+sudo chmod +x /etc/init.d/rpanion-updater
+
 ## and build & run Rpanion
 ./build.sh
 
