39571f2724c73fc64e7118465efcf590eb33ee67
Stephen Dade <stephen_dade@hotmail.com>
2022-03-21 19:56:06 +1100

    Deploy: Mavlink-router fix build on Pi Zero

--- deploy/build_mavlinkrouter.sh
+++ deploy/build_mavlinkrouter.sh
@@ -5,6 +5,13 @@ set -x
 
 cd ../modules/mavlink-router
 meson setup build . --buildtype=release
-ninja -C build
+
+# If less than 500Mb RAM, run with -j2 to not run out of RAM
+if [ $(free -m | awk '/^Mem:/{print $2}') -le 500 ]; then
+    ninja -j 2 -C build
+else
+    ninja -C build
+fi
+
 sudo ninja -C build install
 cd ../../deploy
