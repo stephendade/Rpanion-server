9f3bae5aa1933f006d43a72186d61e8e52c235b4
Stephen Dade <stephen_dade@hotmail.com>
2022-04-28 15:02:48 +1000

    Deploy: Reset any unsaved changes on upgrade

--- deploy/upgrade.sh
+++ deploy/upgrade.sh
@@ -3,6 +3,12 @@
 set -e
 set -x
 
+## Reset any git changes
+git reset --hard
+git submodule foreach --recursive git clean -xfd
+git submodule foreach --recursive git reset --hard
+git submodule update --init --recursive
+
 ## Update source
 cd ../
 git pull
