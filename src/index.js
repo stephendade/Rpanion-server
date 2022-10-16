4dabbd37d40fec5e1fd442c57b8e2b203406ce56
Stephen Dade <stephen_dade@hotmail.com>
2022-10-16 15:16:15 +1100

    Update ReactJS18 style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'startbootstrap-simple-sidebar/dist/css/styles.css'
@@ -8,12 +8,11 @@ import 'startbootstrap-simple-sidebar/dist/css/styles.css'
 import AppRouter from './AppRouter'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(
-  <BrowserRouter>
-    <AppRouter />
-  </BrowserRouter>,
-  document.getElementById('root')
-)
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<BrowserRouter>
+              <AppRouter />
+            </BrowserRouter>)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
