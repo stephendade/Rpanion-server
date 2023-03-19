29bf5cc1925e5bf17aeb41cc57725cec2d6282ce
Stephen Dade <stephen_dade@hotmail.com>
2023-03-19 15:55:58 +1100

    Server: Add tests for indexjs

---
+++ server/index.test.js
@@ -0,0 +1,25 @@
+const assert = require('assert')
+// Import dependencies
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('./index')
+
+// Configure chai
+chai.use(chaiHttp)
+chai.should()
+
+describe('Express server', function () {
+  // Test the GET / endpoint
+  describe('GET /', function() {
+    it('should return a 200 status code', function(done) {
+      chai.request(app)
+        .get('/')
+        .set('Accept', 'application/json')
+        .end(function (err, res) {
+          assert.equal(res.status, 200)
+          assert.equal(err, null)
+          done()
+        })
+    })
+  })
+})
