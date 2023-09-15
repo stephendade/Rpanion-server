413b7d6c098b65f81e8403a7890a70f6d2921489
Stephen Dade <stephen_dade@hotmail.com>
2023-09-15 15:27:58 +1000

    Deploy: Use new nodejs install method

--- deploy/x86-ubuntu20-deploy.sh
+++ deploy/x86-ubuntu20-deploy.sh
@@ -18,7 +18,12 @@ sudo apt install -y wireless-tools
 
 sudo systemctl disable dnsmasq
 
-curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+sudo apt update
 sudo apt install -y nodejs
 
 
