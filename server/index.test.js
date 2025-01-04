7fc2b8b2ac0fc934602c2662d02403f966a8566f
Stephen Dade <stephen_dade@hotmail.com>
2025-01-04 16:17:06 +1100

    Login: Finish GUI and error messages

--- server/index.test.js
+++ server/index.test.js
@@ -22,4 +22,34 @@ describe('Express server', function () {
         })
     })
   })
+
+  // Test that unauthorized users cannot access the /users endpoint
+  describe('GET /users nonauth', function () {
+    it('should return a 401 status code', function (done) {
+      chai.request(app)
+        .get('/users')
+        .set('Accept', 'application/json')
+        .end(function (err, res) {
+          assert.equal(res.status, 401)
+          assert.equal(err, null)
+          done()
+        })
+    })
+  })
+
+  // Test that login works
+  describe('POST /login', function () {
+    it('should return a 200 status code', function (done) {
+      chai.request(app)
+        .post('/login')
+        .send({ username: 'admin', password: 'admin' })
+        .set('Accept', 'application/json')
+        .end(function (err, res) {
+          assert.equal(res.status, 200)
+          assert.equal(err, null)
+          done()
+        })
+    })
+  })
+
 })
--- src/userManagement.js
+++ src/userManagement.js
@@ -1,194 +1,198 @@
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import Table from 'react-bootstrap/Table'
+
 import basePage from './basePage.js'
 
 import './css/styles.css';
-import Form from 'react-bootstrap/Form';
-import Modal from 'react-bootstrap/Modal';
 
 class userManagement extends basePage {
-    constructor (props) {
-        super(props)
-        this.state = {
-        ...this.state,
-        showModal: false,
-        modalType: '',
-        username: '',
-        password: '',
-        users: []
-        }
+  constructor (props) {
+    super(props)
+    this.state = {
+      ...this.state,
+      showModal: false,
+      modalType: '',
+      username: '',
+      password: '',
+      users: []
     }
+  }
     
-    componentDidMount () {
-        this.loadDone();
-        this.fetchUsers();
-    }
+  componentDidMount () {
+    this.loadDone();
+    this.fetchUsers();
+  }
 
-    fetchUsers = async () => {
-        try {
-        const response = await fetch('/users', {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${this.state.token}`
-              },
+  fetchUsers = async () => {
+    try {
+    const response = await fetch('/users', {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${this.state.token}`
+      },
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    this.setState(data);
+    } catch (error) {
+    console.error('Error fetching users:', error);
+    }
+  }
+
+  handleShowModal = (type, username) => {
+    this.setState({ showModal: true, modalType: type, username: username });
+  }
+
+  handleCloseModal = () => {
+    this.setState({ showModal: false, modalType: '', username: '', password: '' });
+  }
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = async () => {
+    const { modalType, username, password } = this.state;
+    if (modalType === 'changePassword') {
+      try {
+        const response = await fetch('/updateUserPassword', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${this.state.token}`
+            },
+          body: JSON.stringify({ username, password })
         });
+        const data = await response.json();
+        this.setState(data);
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+          throw new Error('Network response was not ok');
         }
-        const users = await response.json();
-        this.setState({ users });
-        } catch (error) {
-        console.error('Error fetching users:', error);
+        console.log('Password updated successfully:', data);
+      } catch (error) {
+        console.error('Error updating password:', error);
+      }
+    } else if (modalType === 'addUser') {
+      // Logic to add user
+      try {
+        const response = await fetch('/createUser', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${this.state.token}`
+            },
+          body: JSON.stringify({ username, password })
+        });
+        const data = await response.json();
+        this.setState(data);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
         }
-    }
-
-    handleShowModal = (type, username) => {
-        this.setState({ showModal: true, modalType: type, username: username });
-    }
-
-    handleCloseModal = () => {
-        this.setState({ showModal: false, modalType: '', username: '', password: '' });
-    }
-
-    handleInputChange = (event) => {
-        const { name, value } = event.target;
-        this.setState({ [name]: value });
-    }
-
-    handleSubmit = async () => {
-        const { modalType, username, password } = this.state;
-        if (modalType === 'changePassword') {
-            try {
-                const response = await fetch('/updateUserPassword', {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${this.state.token}`
-                      },
-                    body: JSON.stringify({ username, password })
-                });
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                const data = await response.json();
-                console.log('Password updated successfully:', data);
-            } catch (error) {
-                console.error('Error updating password:', error);
-            }
-        } else if (modalType === 'addUser') {
-            // Logic to add user
-            try {
-                const response = await fetch('/createUser', {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${this.state.token}`
-                      },
-                    body: JSON.stringify({ username, password })
-                });
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                const data = await response.json();
-                console.log('New user added successfully:', data);
-            } catch (error) {
-                console.error('Error adding new user:', error);
-            }
-        } else if (modalType === 'deleteUser') {
-            try {
-                const response = await fetch('/deleteUser', {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${this.state.token}`
-                      },
-                    body: JSON.stringify({ username })
-                });
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                const data = await response.json();
-                console.log('User deleted successfully:', data);
-            } catch (error) {
-                console.error('Error deleting user:', error);
-            }
+        console.log('New user added successfully:', data);
+      } catch (error) {
+        console.error('Error adding new user:', error);
+      }
+    } else if (modalType === 'deleteUser') {
+      try {
+        const response = await fetch('/deleteUser', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${this.state.token}`
+            },
+          body: JSON.stringify({ username })
+        });
+        const data = await response.json();
+        this.setState(data);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
         }
-        this.handleCloseModal();
-        this.fetchUsers();
+        console.log('User deleted successfully:', data);
+      } catch (error) {
+        console.error('Error deleting user:', error);
+      }
     }
+    this.handleCloseModal();
+    this.fetchUsers();
+  }
 
-    renderTitle () {
-        return 'User Management'
-    }
+  renderTitle () {
+    return 'User Management'
+  }
 
-    renderContent () {
-        const isFormValid = this.state.username && (this.state.modalType === 'deleteUser' || (this.state.password && this.state.password === this.state.confirmPassword));
+  renderContent () {
+    const isFormValid = this.state.username && (this.state.modalType === 'deleteUser' || (this.state.password && this.state.password === this.state.confirmPassword));
+    return (
+    <div>
+      <Table id='users' striped bordered hover size="sm">
+      <thead>
+        <tr>
+        <th>Username</th>
+        <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {this.state.users.map(user => (
+        <tr key={user.username}>
+          <td>{user.username}</td>
+          <td>
+          <Button size="sm"className="btn" onClick={() => this.handleShowModal('deleteUser', user.username)}>Delete User</Button>{' '}
+          <Button size="sm" className="btn" onClick={() => this.handleShowModal('changePassword', user.username)}>Change Password</Button>
+          </td>
+        </tr>
+        ))}
+      </tbody>
+      </Table>
 
-        return (
-        <div>
-            
-            <table className="table">
-            <thead>
-            <tr>
-            <th>Username</th>
-            <th>Actions</th>
-            </tr>
-            </thead>
-            <tbody>
-            {this.state.users.map(user => (
-            <tr key={user.username}>
-            <td>{user.username}</td>
-            <td>
-            <Button variant="danger" onClick={() => this.handleShowModal('deleteUser', user.username)}>Delete</Button>
-            <Button variant="primary" onClick={() => this.handleShowModal('changePassword', user.username)}>Change Password</Button>
-            </td>
-            </tr>
-            ))}
-            </tbody>
-            </table>
+      <Button size="sm" className="btn" onClick={() => this.handleShowModal('addUser')}>Add New User</Button>
 
-            <Button variant="success" onClick={() => this.handleShowModal('addUser')}>Add New User</Button>
-
-            <Modal show={this.state.showModal} onHide={this.handleCloseModal}>
-            <Modal.Header closeButton>
-            <Modal.Title>{this.state.modalType === 'changePassword' ? 'Change Password' : this.state.modalType === 'addUser' ? 'Add User' : 'Delete User'}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-            <Form>
-            {this.state.modalType === 'addUser' ? (
-            <Form.Group controlId="formUsername">
-            <Form.Label>Username</Form.Label>
-            <Form.Control type="text" name="username" value={this.state.username} onChange={this.handleInputChange} />
-            </Form.Group>
-            ) : (
-            <Form.Group controlId="formUsername">
-            <Form.Label>Username</Form.Label>
-            <Form.Control plaintext readOnly defaultValue={this.state.username} />
-            </Form.Group>
-            )}
-            {this.state.modalType !== 'deleteUser' && (
-            <Form.Group controlId="formPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" name="password" value={this.state.password} onChange={this.handleInputChange} />
-            <Form.Group controlId="formConfirmPassword">
-            <Form.Label>Confirm Password</Form.Label>
-            <Form.Control type="password" name="confirmPassword" value={this.state.confirmPassword} onChange={this.handleInputChange} />
-            </Form.Group>
-            </Form.Group>
-            )}
-            </Form>
-            </Modal.Body>
-            <Modal.Footer>
-            <Button variant="secondary" onClick={this.handleCloseModal}>Close</Button>
-            <Button variant="primary" onClick={this.handleSubmit} disabled={!isFormValid}>Save Changes</Button>
-            </Modal.Footer>
-            </Modal>
-        </div>
-        )
-    }
+      <Modal show={this.state.showModal} onHide={this.handleCloseModal}>
+      <Modal.Header closeButton>
+        <Modal.Title>{this.state.modalType === 'changePassword' ? 'Change Password' : this.state.modalType === 'addUser' ? 'Add User' : 'Delete User'}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+        {this.state.modalType === 'addUser' ? (
+        <Form.Group controlId="formUsername">
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="text" name="username" value={this.state.username} onChange={this.handleInputChange} />
+        </Form.Group>
+        ) : (
+        <Form.Group controlId="formUsername">
+        <Form.Label>Username</Form.Label>
+        <Form.Control plaintext readOnly defaultValue={this.state.username} />
+        </Form.Group>
+        )}
+        {this.state.modalType !== 'deleteUser' && (
+        <Form.Group controlId="formPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" name="password" value={this.state.password} onChange={this.handleInputChange} />
+        <Form.Group controlId="formConfirmPassword">
+        <Form.Label>Confirm Password</Form.Label>
+        <Form.Control type="password" name="confirmPassword" value={this.state.confirmPassword} onChange={this.handleInputChange} />
+        </Form.Group>
+        </Form.Group>
+        )}
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={this.handleCloseModal}>Cancel</Button>
+        <Button variant="primary" onClick={this.handleSubmit} disabled={!isFormValid}>
+        {this.state.modalType === 'changePassword' ? 'Change Password' : this.state.modalType === 'addUser' ? 'Add User' : 'Delete User'}</Button>
+      </Modal.Footer>
+      </Modal>
+    </div>
+    )
+  }
 }
   
 export default userManagement
