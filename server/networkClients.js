b51bbdf8fd23b8bbce2bc3cdddd7d6bd547461f6
Stephen Dade <stephen_dade@hotmail.com>
2026-01-09 13:47:35 +1100

    Server: Correct permissions for reading network clients

--- server/networkClients.js
+++ server/networkClients.js
@@ -25,7 +25,7 @@ function getClients (callback) {
               // 1606808691 34:7d:f6:65:b1:1b 10.0.2.117 l5411 01:34:7d:f6:65:b1:1b
               // we have an active AP
               const allclients = []
-              const out = fs.readFileSync('/var/lib/NetworkManager/dnsmasq-' + device + '.leases')
+              const out = execSync('sudo cat /var/lib/NetworkManager/dnsmasq-' + device + '.leases')
               const allleases = out.toString().split('\n')
               for (let j = 0, lenn = allleases.length; j < lenn; j++) {
                 if (allleases[j] !== '') {
